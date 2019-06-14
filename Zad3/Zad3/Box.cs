b441dc1e187829e1af262c2d4a0c16c1c271237a
Kawwolski <47087721+Kawwolski@users.noreply.github.com>
2019-06-14 15:31:22 +0200

    Add files via upload

---
+++ Zad3/Zad3/Box.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad3
+{
+   public class Box
+    {
+        public double width { get; set; }
+        public double height { get; set; }
+        public double depth { get; set; }
+
+        public double GetVolume(double w,double h,double d)
+        {
+            return w*h*d;
+        }
+        public double GetArea(double w,double h,double d)
+        {
+            return 2 * (w * d + h * d + w * h);
+        }
+
+        public override string ToString()
+        {
+            return "W:" + width + " H:" + height+" D:"+depth;
+        }
+    }
+}
---
+++ Zad3/Zad3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double maxvolume = 250,currentvolume=0;
+            List<Box> list = new List<Box>();
+            do
+            {
+                Box box = new Box();
+                Console.WriteLine("Podaj szerokosc ");
+                box.width = double.Parse(Console.ReadLine());
+                Console.WriteLine("Podaj glebokosc ");
+                box.depth = double.Parse(Console.ReadLine());
+                Console.WriteLine("Podaj wysokosc: ");
+                box.height = double.Parse(Console.ReadLine());
+                list.Add(box);
+                currentvolume += box.GetVolume(box.width, box.height, box.depth);
+
+            } while (currentvolume <= maxvolume);
+            Console.WriteLine(currentvolume.ToString());
+            foreach(var b in list)
+            {
+                Console.WriteLine(b);
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
